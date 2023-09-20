829736887c04951450829d48ddccd8519e245782
audgns722 <audgns722@gmail.com>
2023-09-20 18:01:44 +0900

    fix

--- slider/js/common.js
+++ slider/js/common.js
@@ -1,6 +1,6 @@
 hljs.highlightAll();
 
-//  모달
+// 모달
 
 const modalBtn = document.querySelector(".modal__btn");
 const modalClose = document.querySelector(".modal__close");
@@ -9,24 +9,24 @@ const modalCont = document.querySelector(".modal__cont");
 modalBtn.addEventListener("click", () => {
     modalCont.classList.add("show");
     modalCont.classList.remove("hide");
-})
+});
+
 modalClose.addEventListener("click", () => {
     modalCont.classList.add("hide");
-})
-
+});
 
 // 탭메뉴
 const tabBtn = document.querySelectorAll(".modal__box .tabs > div");
 const tabCont = document.querySelectorAll(".modal__box .cont > div");
 
 tabBtn.forEach((element, index) => {
-    element,addEventListener("click", (e) => {
+    element.addEventListener("click", (e) => {
         e.preventDefault();
 
         tabBtn.forEach(li => li.classList.remove("active"));
         element.classList.add("active");
 
-        tabCont.forEach(div => div.computedStyleMap.display = "none");
+        tabCont.forEach(div => div.style.display = "none");
         tabCont[index].style.display = "block";
-    })
-})
+    });
+});
